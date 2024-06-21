7a62b77e7ad728842bc7c0f6fc2360ad3088c981
Buddy <39768980+budrice@users.noreply.github.com>
2024-06-21 19:39:05 -0400

    lesson 123
    lesson 123

---
+++ starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,35 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.css',
+  host: {
+    id: 'status'
+  }
+})
+export class ServerStatusComponent implements OnInit {
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+  statusArray : ('online' | 'offline' | 'unknown')[] = ['unknown', 'online', 'offline'];
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.cycleStatus();
+  }
+
+  cycleStatus() {
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i === 3) { i = 0; }
+      this.currentStatus.set(this.statusArray[i]);
+      i++;
+    }, 15000);
+    this.destroyRef.onDestroy(() => {
+      clearTimeout(interval);
+    });
+  }
+
+}
---
+++ starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from "../../../shared/button/button.component";
+import { ControlComponent } from "../../../shared/control/control.component";
+
+@Component({
+    selector: 'app-new-ticket',
+    standalone: true,
+    templateUrl: './new-ticket.component.html',
+    styleUrl: './new-ticket.component.css',
+    imports: [ButtonComponent, ControlComponent, FormsModule],
+    host: {
+        id: 'new-ticket'
+    }
+})
+export class NewTicketComponent {
+    onSubmit() {
+        console.log('submitted');
+    }
+}
